bc7d13a3a40931ae6cd86d4f862235c42c637dca
limjimin <jiaezzan@naver.com>
2021-01-29 16:36:32 +0900

    first

---
+++ Java_001/src/com/callor/hello/HelloTimes.java
@@ -0,0 +1,29 @@
+package com.callor.hello;
+
+public class HelloTimes {
+
+	public static void main(String[] args) {
+		System.out.println("3 x 1 = " + 3 * 1);
+		System.out.println("3 x 1 = " + 3 * 2);
+		System.out.println("3 x 1 = " + 3 * 3);
+		System.out.println("3 x 1 = " + 3 * 4);
+		System.out.println("3 x 1 = " + 3 * 5);
+		System.out.println("3 x 1 = " + 3 * 6);
+		System.out.println("3 x 1 = " + 3 * 7);
+		System.out.println("3 x 1 = " + 3 * 8);
+		System.out.println("3 x 1 = " + 3 * 9);
+		
+		System.out.println("6 x 1 = " + 6 * 1);
+		System.out.println("6 x 1 = " + 6 * 2);
+		System.out.println("6 x 1 = " + 6 * 3);
+		System.out.println("6 x 1 = " + 6 * 4);
+		System.out.println("6 x 1 = " + 6 * 5);
+		System.out.println("6 x 1 = " + 6 * 6);
+		System.out.println("6 x 1 = " + 6 * 7);
+		System.out.println("6 x 1 = " + 6 * 8);
+		System.out.println("6 x 1 = " + 6 * 9);
+		
+		
+	}
+
+}
